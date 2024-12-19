85114b15815acfe668791dea8ee3873096c98480
Yulo97 <giuliano_devito@hotmail.com>
2024-12-19 20:33:28 -0300

    reparacion

--- conexionFrontYBack/server/src/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const productControllers = require("../controllers/product.controllers.js");
-
-const router = Router();
-
-router.get("/", productControllers.getAllProducts);
-router.delete("/", productControllers.deleteProductById);
-
-module.exports = router;
